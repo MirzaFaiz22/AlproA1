36519593e927edfb9e453556ea871ee42d2b1a66
MirzaFaiz22 <117134699+MirzaFaiz22@users.noreply.github.com>
2023-03-21 08:26:12 +0700

    Add files via upload

---
+++ FrekNilTabel.c
@@ -0,0 +1,45 @@
+/* Nama File    		: FrekNilTabel.c */
+/* Deskripsi    		: Mencari nilai elemen tabel T yang muncul lebih dari 1
+/* Pembuat      		: Muhammad Mirza Faiz Rabbani - 24060122140127 */
+/* Tanggal Pembuatan    : 21 maret 2023 */	
+
+int main () {
+    /* Kamus */
+    int N, i, j, frekuensi;
+    int T[100], a[100];
+
+    /* Algoritma*/
+
+    printf("Nilai N = ");
+    scanf("%d", &N);
+
+    for (i = 0; i < N; i++){
+        printf("T[%d] = ", i);
+        scanf("%d",&T[i]);
+        a[i] = 1;
+    }
+    
+    for (i = 0; i < N; i++){
+        frekuensi = 1;
+        for (j = i + 1; j < N; j++){
+            if (T[i] == T[j]){
+                frekuensi++;
+                a[j] = 0;
+            }
+        }
+        if (a[i] != 0){
+            a[i] = frekuensi;
+        }        
+    }
+    for (i = 0; i < N; i++){
+        if (a[i] > 1){
+            printf("%d ", T[i]);
+        }
+        
+    }
+
+        
+
+    return 0;
+
+}
